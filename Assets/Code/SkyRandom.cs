b2c713e105668ece846a712a5f938cdb2608960c
Niti Thammarerkrit <smallnick007@hotmail.com>
2018-03-20 23:54:44 +0700

    Add [FULL] Heritia Town!!!!!

---
+++ Assets/Code/SkyRandom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkyRandom : MonoBehaviour {
+
+    public const int num_sky = 3;
+    public Sprite[] sky = new Sprite[num_sky];
+
+	void Start () {
+        int ran = Random.Range(0,num_sky);
+        this.GetComponent<SpriteRenderer>().sprite = sky[ran];	
+	}
+	
+}
